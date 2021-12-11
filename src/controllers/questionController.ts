d1bd209a28d62e4e24abcdde915e39acc75d59c5
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-11 01:43:37 -0300

    Do some eslint fixes

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-unresolved */
 import { Request, Response } from 'express';
 import * as questionRepository from '../repositories/questionRepository';
