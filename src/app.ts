9febd21e1bf89a8f6e6e9ea57b0e72ae44b5e533
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-10 23:10:11 -0300

    Add the create user function as a post request to /users

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
+/* eslint-disable import/no-unresolved */
 import express from 'express';
 import cors from 'cors';
+import * as userController from './controllers/userController';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/users', userController.createUser);
 
 export default app;
